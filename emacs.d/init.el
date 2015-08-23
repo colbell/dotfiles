ac29408a1d0be4fad2a9968e7e50b91909b82e5e
Colin Bell <col@baibell.org>
2015-08-23 12:40:05 +1000

    EMACS

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -10,6 +10,13 @@
 ;; Use org-babel-load-file to compile colbell.org into colbell.el and
 ;; then load colbell.el. Only do this if colbell.el doesn't exist or
 ;; if colbell.org is newer than colbell.el.
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;(package-initialize)
+
 (let ((my-el-f (expand-file-name "colbell.el" user-emacs-directory))
       (my-org-f (expand-file-name "colbell.org" user-emacs-directory)))
   (if (or (not (file-exists-p my-el-f))
