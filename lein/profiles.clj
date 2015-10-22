026e7f22a1ed064ff956c3076a63ee8d3380f78b
Colin Bell <col@baibell.org>
2015-10-22 20:32:23 +1100

    lein

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user
- { :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"] ; nREPL middleware for cider
+ { :plugins [[cider/cider-nrepl "0.9.1"] ; nREPL middleware for cider
              [lein-ancient "0.6.7"]               ; Looks for old libs
              [lein-annotations "0.1.0"]           ; Displays comment annotations
                                                   ; in proj
@@ -7,9 +7,9 @@
              [lein-bikeshed "0.2.0"]              ; Code smells
              [jonase/eastwood "0.2.1"]            ; CLJ lint tool
              [lein-kibit "0.1.2"]                 ; Static code analyzer
-             [refactor-nrepl "1.0.5"]             ; Refactoring tools
-             [codox "0.8.12"]                     ; Generate API doc
-             [lein-midje "3.1.3"]                 ; Testing framework
+             [refactor-nrepl "1.2.0"]             ; Refactoring tools
+             [codox "0.9.0"]                     ; Generate API doc
+             [lein-midje "3.2"]                 ; Testing framework
              [lein-pprint "1.1.2"]                ; Pretty pint
              [com.aphyr/prism "0.1.3"]]           ; Autorun tests
 
