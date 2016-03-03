407bacd6e7d23e1d8a29be0f7ef5dcee13abfbe9
Colin Bell <col@baibell.org>
2016-03-03 12:47:21 +1100

    Spacemacs

---
+++ spacemacs.d/layers/cnb-dired-narrow/packages.el
@@ -0,0 +1,23 @@
+;;; packages.el --- Visual Bookmarks Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2015 Colin Bell
+;;
+;; Author: Colin Bell <col@baibell.org>
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defvar cnb-dired-narrow-packages '(dired-hacks-utils dired-narrow))
+
+;; List of packages to exclude.
+(setq cnb-bm-excluded-packages '())
+
+(defun cnb-dired-narrow/init-dired-hacks-utils ()
+  (use-package dired-hacks-utils
+    :ensure t))
+ 
+(defun cnb-dired-narrow/init-dired-narrow ()
+  (use-package dired-narrow
+    :ensure t))
+ 
