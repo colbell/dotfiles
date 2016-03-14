441495c0ec27e62e63090635edb4d8649618c70c
Colin Bell <col@baibell.org>
2016-03-15 10:46:53 +1100

    Upgrade lein profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,11 +2,11 @@
  { :plugins [[lein-ancient "0.6.8"]               ; Looks for old libs
              [lein-annotations "0.1.0"]           ; Displays comment annotations
              [lein-marginalia "0.8.0"]            ; Document CLJ
-             [lein-bikeshed "0.2.0"]              ; Code smells
+             [lein-bikeshed "0.3.0"]              ; Code smells
              [jonase/eastwood "0.2.3"]            ; CLJ lint tool
              [lein-kibit "0.1.2"]                 ; Static code analyzer
-             [refactor-nrepl "1.2.0"]             ; Refactoring tools
-             [codox "0.9.1"]                      ; Generate API doc
+             [refactor-nrepl "2.0.0"]             ; Refactoring tools
+             [codox "0.9.4"]                      ; Generate API doc
              [lein-midje "3.2"]                   ; Testing framework
              [lein-pprint "1.1.2"]                ; Pretty pint
              [com.aphyr/prism "0.1.3"]]           ; Autorun tests
@@ -16,7 +16,7 @@
 
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}}
 
- :repl {:plugins [[cider/cider-nrepl "0.10.0"]]}
+ :repl {:plugins [[cider/cider-nrepl "0.11.0"]]}
 
  ;; :repl-options
  ;; { :init
