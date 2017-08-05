3b1fbced251e9abadf5ef0f3a7a713f0a0591e8b
Colin Bell <col@baibell.org>
2017-08-05 20:17:42 +1000

    spacemacs

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -81,7 +81,7 @@ values."
                       version-control-diff-tool 'diff-hl
                       version-control-global-margin t)
      vimscript
-     tbuckyaml)
+     yaml)
 
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
