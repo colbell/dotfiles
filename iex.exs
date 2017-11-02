0f11ddbc5ef32b41f042e516f0a25e1179240a3d
Colin Bell <col@baibell.org>
2017-11-02 20:06:57 +1100

    iex.exs

--- iex.exs
+++ iex.exs
@@ -1,4 +1,21 @@
 IEx.configure(
-  history_size: 9999,
-  inspect: [limit: 10, pretty: true, char_lists: :as_lists]
+  history_size: 500,
+  inspect: [limit: :infinity, pretty: true, charlists: :as_lists],
+  default_prompt:
+  "#{IO.ANSI.green}%prefix#{IO.ANSI.reset}" <>
+    "[#{IO.ANSI.cyan}%counter#{IO.ANSI.reset}]>",
+  alive_prompt:
+  "#{IO.ANSI.green}%prefix#{IO.ANSI.reset}" <>
+    "(#{IO.ANSI.yellow}%node#{IO.ANSI.reset})" <>
+    "[#{IO.ANSI.cyan}%counter#{IO.ANSI.reset}]>",
 )
+
+# Add to a project specific .iex.exs
+# File.exists?(Path.expand("~/.iex.exs")) && import_file("~/.iex.exs")
+
+# import Ecto.Query
+
+# alias Project.Repo
+
+# alias Project.User
+# alias Project.Post
