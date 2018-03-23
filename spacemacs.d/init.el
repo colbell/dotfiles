d8d8302d32fc7753d38b031806e9f2c848db815a
Colin Bell <col@baibell.org>
2018-03-23 15:59:57 +1100

    Spacemacs

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -90,9 +90,7 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages then consider to create a layer, you can also put the
    ;; configuration in 'dotspacemacs/config'.
-   dotspacemacs-additional-packages '(gruvbox-theme
-                                      moe-theme
-                                      diredfl
+   dotspacemacs-additional-packages '(diredfl
                                       ;; FIXME: Do I need this?
                                       peep-dired
 
@@ -185,6 +183,7 @@ It should only modify the values of Spacemacs settings."
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(
                          gruvbox-dark-hard
+                         gruvbox-light-hard
                          leuven
                          solarized-dark
                          solarized-light
@@ -195,7 +194,8 @@ It should only modify the values of Spacemacs settings."
    ;; user defined themes, refer to the DOCUMENTATION.org for more info on how
    ;; to create your own spaceline theme.. (default 'spacemacs)
    ;; dotspacemacs-mode-line-theme '(all-the-icons :separator wave :separator-scale 2.0)
-   dotspacemacs-mode-line-theme '(spacemacs)
+   ;; dotspacemacs-mode-line-theme '(spacemacs)
+   dotspacemacs-mode-line-theme '(vim-powerline)
 
    ;; If non nil the cursor color matches the state color.
    dotspacemacs-colorize-cursor-according-to-state t
@@ -441,6 +441,10 @@ layers configuration. You are free to put any user code."
 
   ;; (spacemacs/toggle-transparency)
 
+  ;; Fixes problems with code reloading not working in Elixir/Phoenix.
+  ;; See http://spacemacs.org/doc/FAQ.html#orgheadline18
+  (setq create-lockfiles nil)
+
   ;;==============================================
   ;; Rainbow mode
   ;;==============================================
@@ -594,8 +598,9 @@ layers configuration. You are free to put any user code."
   ;;==============================================
   (setq alchemist-test-status-modeline t)
   (setq alchemist-test-display-compilation-output t)
-  (setq alchemist-hooks-test-on-save t)
-  (setq alchemist-hooks-compile-on-save t)
+  ;; (setq alchemist-hooks-test-on-save t)
+  ;; (setq alchemist-hooks-compile-on-save t)
+  (setq flycheck-elixir-credo-strict t)
 
   ;;==============================================
   ;; Web mode configuration
@@ -776,7 +781,7 @@ This function is called at the very end of Spacemacs initialization."
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (web-mode org-mime org-download hy-mode evil-org color-identifiers-mode company flycheck helm helm-core ghub inf-ruby treemacs dash yasnippet use-package org-plus-contrib yasnippet-snippets yapfify yaml-mode xterm-color ws-butler winum which-key wgrep web-beautify w3m volatile-highlights vi-tilde-fringe uuidgen treemacs-projectile treemacs-evil toc-org tagedit systemd symon string-inflection sql-indent spaceline-all-the-icons solarized-theme smex smeargle slim-mode shrink-whitespace shell-pop scss-mode sass-mode rvm ruby-tools ruby-test-mode ruby-refactor ruby-hash-syntax rubocop rspec-mode robe restart-emacs request rbenv rainbow-mode rainbow-identifiers rainbow-delimiters pyvenv pytest pyenv-mode py-isort pug-mode projectile-rails popwin pippel pipenv pip-requirements pfuture persp-mode persistent-scratch peep-dired pdf-tools password-generator paradox overseer orgit org-projectile org-present org-pomodoro org-bullets org-brain open-junk-file ob-elixir nlinum-relative nameless multi-term move-text moe-theme mmm-mode minitest markdown-toc magit-gitflow macrostep lorem-ipsum livid-mode live-py-mode link-hint json-mode js2-refactor js-doc ivy-xref ivy-rich ivy-purpose ivy-hydra insert-shebang indent-guide importmagic impatient-mode ibuffer-projectile hungry-delete ht hl-todo highlight-parentheses highlight-numbers highlight-indentation helm-make gruvbox-theme google-translate golden-ratio gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ gh-md ggtags fuzzy font-lock+ flyspell-correct-ivy flycheck-pos-tip flycheck-mix flycheck-elm flycheck-credo flycheck-bashate flx-ido fish-mode fill-column-indicator feature-mode fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-cleverparens evil-args evil-anzu eval-sexp-fu eshell-z eshell-prompt-extras esh-help erlang emmet-mode elm-mode elisp-slime-nav editorconfig dumb-jump diredfl dired-narrow diminish diff-hl define-word cython-mode csv-mode counsel-projectile counsel-gtags counsel-css company-web company-tern company-statistics company-shell company-quickhelp company-lua company-anaconda column-enforce-mode coffee-mode clean-aindent-mode chruby centered-cursor-mode bundler browse-at-remote bm bind-key auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile alchemist aggressive-indent adaptive-wrap ace-window ace-link ac-ispell)))
+    (erlang auto-compile helm git-commit org-plus-contrib yasnippet-snippets yapfify yaml-mode xterm-color ws-butler winum which-key wgrep web-mode web-beautify w3m volatile-highlights vi-tilde-fringe uuidgen use-package treemacs-projectile treemacs-evil toc-org tagedit systemd symon string-inflection sql-indent spaceline-all-the-icons solarized-theme smex smeargle slim-mode shrink-whitespace shell-pop scss-mode sass-mode rvm ruby-tools ruby-test-mode ruby-refactor ruby-hash-syntax rubocop rspec-mode robe restart-emacs request rbenv rainbow-mode rainbow-identifiers rainbow-delimiters pyvenv pytest pyenv-mode py-isort pug-mode projectile-rails popwin pippel pipenv pip-requirements persp-mode persistent-scratch peep-dired pdf-tools password-generator paradox packed overseer orgit org-projectile org-present org-pomodoro org-mime org-download org-bullets org-brain open-junk-file ob-elixir nlinum-relative nameless multi-term move-text mmm-mode minitest markdown-toc magit-gitflow macrostep lorem-ipsum livid-mode live-py-mode link-hint json-mode js2-refactor js-doc ivy-xref ivy-rich ivy-purpose ivy-hydra insert-shebang indent-guide importmagic impatient-mode ibuffer-projectile hy-mode hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation helm-make helm-core gruvbox-theme google-translate golden-ratio gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ gh-md ggtags fuzzy font-lock+ flyspell-correct-ivy flycheck-pos-tip flycheck-mix flycheck-elm flycheck-credo flycheck-bashate flx-ido fish-mode fill-column-indicator feature-mode fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-org evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-cleverparens evil-args evil-anzu eval-sexp-fu eshell-z eshell-prompt-extras esh-help emmet-mode elm-mode elisp-slime-nav editorconfig dumb-jump diredfl dired-narrow diminish diff-hl define-word cython-mode csv-mode counsel-projectile counsel-gtags counsel-css company-web company-tern company-statistics company-shell company-quickhelp company-lua company-anaconda column-enforce-mode color-identifiers-mode coffee-mode clean-aindent-mode chruby centered-cursor-mode bundler browse-at-remote bm auto-yasnippet auto-highlight-symbol auto-dictionary alchemist aggressive-indent adaptive-wrap ace-link ac-ispell)))
  '(tramp-syntax (quote default) nil (tramp)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
