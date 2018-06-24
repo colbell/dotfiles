0732631dbd51a889d8fc3265e3c812a07b618cdd
Colin Bell <col@baibell.org>
2018-06-24 18:13:20 +1000

    Emacs

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -247,6 +247,7 @@ It should only modify the values of Spacemacs settings."
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(
                          leuven
+                         flatland
                          gruvbox-dark-hard
                          spacemacs-dark
                          spacemacs-light
