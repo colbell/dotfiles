f273c99d4735b3ce63330752e0aeacfb33da539d
Colin Bell <col@baibell.org>
2018-09-30 18:11:48 +1000

    emacs

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -255,6 +255,8 @@ It should only modify the values of Spacemacs settings."
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(
+                         doom-one
+                         doom-one-light
                          tsdh-dark
                          tsdh-light
                          flatland
