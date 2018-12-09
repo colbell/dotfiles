d084b99ac162a5508bc347b2a6bb95d4fa0f9999
Colin Bell <col@baibell.org>
2018-12-09 11:37:08 +1100

    psql

--- psqlrc-aliases/locks.sql
+++ psqlrc-aliases/locks.sql
@@ -3,6 +3,6 @@ SELECT bl.pid AS blocked_pid, a.usename AS blocked_user, kl.pid AS blocking_pid,
   FROM pg_catalog.pg_locks bl
          JOIN pg_catalog.pg_stat_activity a ON bl.pid = a.pid
          JOIN pg_catalog.pg_locks kl
-         JOIN pg_catalog.pg_stat_activity ka ON kl.pid = ka.pid ON bl.transactionid = kl.transactionid AND
-              bl.pid != kl.pid
+         JOIN pg_catalog.pg_stat_activity ka
+             ON kl.pid = ka.pid ON bl.transactionid = kl.transactionid AND bl.pid != kl.pid
  WHERE NOT bl.granted;
