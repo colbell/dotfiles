5c84a122dcaab4c8d32f3184e32cdd77d6f338a6
Colin Bell <col@baibell.org>
2018-12-11 19:59:50 +1100

    Emacs org -> el

--- spacemacs.d/snippets/ruby-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `ruby-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'ruby-mode
-                     '(("tt" "<tt>$0</tt>" "tt" nil nil nil "/home/colbell/.spacemacs.d/snippets/ruby-mode/tt" nil nil)))
-
-
-;;; Do not edit! File generated at Tue Jan  5 15:44:47 2016
